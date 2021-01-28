18ae80af21fe2c2c690ea32922cad308fbc1e68a
Pavan-kumar261 <pavan.kumar@nineleaps.com>
2021-01-28 15:41:27 +0530

    Implemeted firebase utils , included ability to store authenticated users into firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,13 +12,50 @@ const config = {
   measurementId: "G-3BEPXKWSF7",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  // Getting the reference object of the user with id
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // Getting the snapshot object (basically data) of the user reference
+  const snapshot = await userRef.get();
+
+  // If data does not exist we are creating the data which we require to store in the db
+  if (!snapshot.exists) {
+    
+    // Taking displayName and email properties from the google object when we sign in
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// Creating a new google auth provider with GoogleAuthProvider class from firebase auth
 const provider = new firebase.auth.GoogleAuthProvider();
+
+// This makes the sign window pop-up everytime we click on sign in with google button
 provider.setCustomParameters({ prompt: "select_account" });
+
+// signInWithPopup method triggers the pop-up. So to tell it to pop-up the google related pop-up we provide our provider as an argument to it.
+// This usually provides various pop-ups depending on the provider argument passed to it.
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
