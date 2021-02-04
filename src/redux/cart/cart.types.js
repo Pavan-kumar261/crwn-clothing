e452a019043ddf719a32e07ca7a93d50eea7bc92
Pavan-kumar261 <pavan.kumar@nineleaps.com>
2021-02-04 17:32:38 +0530

    Updated shop data with new data, updated cart utils to group items in cart

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
-const { act } = require("react-dom/test-utils");
-
-export const CartActionTypes = {
+const CartActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+  ADD_ITEM: "ADD_ITEM",
 };
+
+export default CartActionTypes;
